8a6d6b8e2474809f41eba4557fcd3db22f9a614b
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-13 00:37:38 +0300

    unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- create a table users if it doesnt exist
+-- with specified attributes
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
