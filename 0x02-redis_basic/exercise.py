2761f94fd57ec085d093812c054eea1304c09ee9
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-18 22:04:19 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 """writing strings to redis"""
-#import redis
+import redis
 import uuid
-
 
 
 class cache:
     """A class cache using redis"""
-    
-    
     def __init__(self):
         """initialize the cache by creating a redis client
         and flushing the database"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-        
-        
+
     def store(self, data) -> str:
         """store the input data in Redis with a randomly generated key
         and return the key"""
