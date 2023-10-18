ffbbe6d7c2f605d09abebb47d6bf2ea9e6581db9
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-18 22:16:37 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """writing strings to redis"""
 import redis
 import uuid
-
+from typing import Callable, Optional
 
 class cache:
     """A class cache using redis"""
@@ -18,3 +18,18 @@ class cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """Retrieve data from the cache using the provided key"""
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str):
+        """Retrieve a string from the cache"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    
+    def get_int(self, key: str):
+        """Retrieve an integer from the cache"""
+        return self.get(key, fn=int)
