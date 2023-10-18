2b630cf5dca0f83cedd1b714d5de3a62be269fb1
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-18 22:59:38 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -29,6 +29,7 @@ class cache:
 
     def call_history(method: Callable) -> Callable:
         """Decorator to store the history of inputs and outputs"""
+        @wraps(method)
         def wrapper(self, *args, **kwargs):
             """wrapper function"""
             input_key = "{}:inputs".format(method.__qualname__)
